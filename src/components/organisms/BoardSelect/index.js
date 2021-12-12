be87e6fdcbba6f36034295620c3d20b2fdac139b
Jose Luiz Dos Santos Xavier <joluizsantos231293@gmail.com>
2021-12-12 18:40:15 -0300

    :construction: description of selector cards

--- src/components/organisms/BoardSelect/index.js
+++ src/components/organisms/BoardSelect/index.js
@@ -1,29 +1,50 @@
-import ImageGlobal from '../../../helpers/ImageGlobal';
-import Img from '../../atoms/Img';
-import NewElement from '../../atoms/NewElement';
+import ImageGlobal from "../../../helpers/ImageGlobal";
+import Img from "../../atoms/Img";
+import NewElement from "../../atoms/NewElement";
 
 export default () => {
   const BoardSelect = NewElement("board-select", "", "section");
 
-  const CardSun = NewElement("card-select", "", "aside");  
-  const TextCardSun = NewElement("text-card-sun", "", "p")
-  TextCardSun.append(NewElement("text-bold", "1. ", "span") , "Set the amount of", NewElement("text-bold", " sunlight ", "span"),  "your plant will get.");  
-  CardSun.append(Img("icon-card-select", ImageGlobal.iconSun, "icon Sun"));
-  CardSun.append(TextCardSun);
- 
+  const CardSun = NewElement("card-select", "", "aside");
+  const TextCardSun = NewElement("text-card", "", "p");
+  TextCardSun.append(
+    NewElement("text-bold", "1. ", "span"),
+    "Set the amount of",
+    NewElement("text-bold", " sunlight ", "span"),
+    "your plant will get."
+  );
+  CardSun.append(
+    Img("icon-card-select", ImageGlobal.iconSun, "icon Sun"),
+    TextCardSun
+  );
 
-
-  const CardWater = NewElement("card-select", "", "aside"); 
-  CardWater.append(Img("icon-card-select", ImageGlobal.iconWater,"icon Water"));
-
+  const CardWater = NewElement("card-select", "", "aside");
+  const TextCardWater = NewElement("text-card", "", "p");
+  TextCardWater.append(
+    NewElement("text-bold", "2. ", "span"),
+    "How often do you want to",
+    NewElement("text-bold", " water ", "span"),
+    "your plant?"
+  );
+  CardWater.append(
+    Img("icon-card-select", ImageGlobal.iconWater, "icon Water"),
+    TextCardWater
+  );
 
   const CardDog = NewElement("card-select", "", "aside");
-  CardDog.append(Img("icon-card-select", ImageGlobal.iconChew,"icon Chew"));
- 
+  const TextCardDog = NewElement("text-card-dog", "", "p");
+  TextCardDog.append(
+    NewElement("text-bold", "3. ", "span"),
+    "Do you have pets? Do they",
+    NewElement("text-bold", " chew ", "span"),
+    "plants?"
+  );
+  CardDog.append(
+    Img("icon-card-select", ImageGlobal.iconChew, "icon Chew"),
+    TextCardDog
+  );
 
-
-  
-BoardSelect.append(CardSun, CardWater, CardDog);
+  BoardSelect.append(CardSun, CardWater, CardDog);
 
   return BoardSelect;
 };
